d46193744cb828381ad14be6c0e6e15445e602ac
vanppo <vanppo@hotmail.com>
2021-05-07 12:12:15 +0800

    Migrate to vue 3

--- packages/vue/index.js
+++ packages/vue/index.js
@@ -3,10 +3,7 @@ module.exports = {
     es2020: true,
   },
   extends: [
-    './rules/base',
-    './rules/essential',
-    './rules/miscellaneous',
-    './rules/readability',
+    'plugin:vue/vue3-essential',
   ],
   parser: 'vue-eslint-parser',
   parserOptions: {
@@ -15,4 +12,41 @@ module.exports = {
   plugins: [
     'eslint-plugin-vue',
   ],
+  rules: {
+    // vue 3 strongly recommended
+    'vue/attribute-hyphenation': 'error',
+    'vue/component-definition-name-casing': ['error', 'PascalCase'],
+    'vue/html-closing-bracket-newline': 'error',
+    'vue/html-closing-bracket-spacing': 'error',
+    'vue/html-end-tags': 'error',
+    'vue/html-indent': 'error',
+    'vue/html-quotes': 'error',
+    'vue/html-self-closing': 'error',
+    'vue/max-attributes-per-line': 'error',
+    'vue/multiline-html-element-content-newline': 'error',
+    'vue/mustache-interpolation-spacing': 'error',
+    'vue/no-multi-spaces': 'error',
+    'vue/no-spaces-around-equal-signs-in-attribute': 'error',
+    'vue/no-template-shadow': 'error',
+    'vue/one-component-per-file': 'error',
+    'vue/prop-name-casing': 'error',
+    'vue/require-default-prop': 'error',
+    'vue/require-explicit-emits': 'error',
+    'vue/require-prop-types': 'error',
+    'vue/v-bind-style': 'error',
+    'vue/v-on-style': 'error',
+    'vue/v-slot-style': 'error',
+
+    // vue 3 recommended
+    'vue/attributes-order': 'error',
+    'vue/component-tags-order': ['error', { 'order': ['template', 'script', 'style'] }],
+    'vue/no-lone-template': 'error',
+    'vue/no-multiple-slot-args': 'error',
+    'vue/no-v-html': 'error',
+    'vue/order-in-components': 'error',
+    'vue/this-in-template': 'error',
+
+    // MISC
+    'vue/static-class-names-order': 'error',
+  }
 }
